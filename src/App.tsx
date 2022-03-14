41fd306292737418079462a87a20847d61096fdb
Luiz Satto <luizf.satto@gmail.com>
2022-03-14 18:02:19 -0300

    Added app footer

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,15 @@ const App: React.FC = () => {
                 </Layout>
             </div>
             <div className='footer'>
-
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                    Pandora Crypto <br />
+                    All rights reserved.
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
             </div>
         </div>
     )
